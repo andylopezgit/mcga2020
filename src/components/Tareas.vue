c54e47f4c4376b4622644b8304a67f3508f8507a
andylopezgit <alopezciotta@gmail.com>
2021-06-15 10:29:16 -0300

    agrego tarjetas para agregar tareas y listado de tareas

--- src/components/Tareas.vue
+++ src/components/Tareas.vue
@@ -1,11 +1,68 @@
 <template>
-    <div>
-        <h2>Tareas</h2>
-    </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card elevation-10 width="900px">
+          <v-card-title primary-title>
+            Manejo de Tareas
+          </v-card-title>
+          <v-row justify="center" align="center">
+            <v-col cols="6">
+              <v-card-text>
+                <v-text-field
+                  name="name"
+                  label="Tarea"
+                  v-model="task"
+                ></v-text-field>
+              </v-card-text>
+            </v-col>
+            <v-col cols="4">
+              <v-card-text>
+                <v-autocomplete
+                label="Ciudad"
+                v-model="city"
+                :items="cities"
+                >
+                    
+                    
+                </v-autocomplete>
+              </v-card-text>
+            </v-col>
+            <v-col cols="2">
+              <v-btn class="primary">Agregar</v-btn>
+            </v-col>
+          </v-row>
+
+          <v-card-actions> </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row
+        justify="center">
+        <v-col cols="10">
+            <v-card
+                elevation-10
+                width="800px"
+            >
+                <v-card-title primary-title>
+                    Listado de Tareas
+                </v-card-title>
+
+            </v-card>
+        </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name:"Tareas"
-}
+  name: "Tareas",
+  data() {
+      return {
+          cities: ['Rosario', 'Cordoba', 'Capital'],
+          city: '',
+          task: ''
+      }
+  },
+};
 </script>
